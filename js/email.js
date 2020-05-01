1413649e3a68b0d18900a3ade6bbf776e4352a53
Uriel-David <uriel.david.qaa@gmail.com>
2020-05-01 02:46:07 -0300

    Adição da página contato.html e separação dos css em vários arquivos para melhorar o controle de mundanças e evitar sobreposição de estilos, adição da biblioteca PHP-Mailer ao projeto.

--- js/email.js
+++ js/email.js
@@ -6,7 +6,7 @@ function validaForm(frm) {
     */
     //Verifica se o campo nome foi preenchido e
     //contém no mínimo três caracteres.
-    if(frm.nome.value == "" || frm.nome.value == null || frm.nome.value.length < 3) {
+    if(frm.nome.value === "" || frm.nome.value == null || frm.nome.value.length < 3) {
     //É mostrado um alerta, caso o campo esteja vazio.
     alert("Por favor, indique o seu nome.");
     //Foi definido um focus no campo.
@@ -14,59 +14,26 @@ function validaForm(frm) {
     //o form não é enviado.
     return false;
     }
+
     //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
-    if(frm.email.value.indexOf("@") == -1 ||
-        frm.email.valueOf.indexOf(".") == -1 ||
-        frm.email.value == "" ||
+    if(frm.email.value.indexOf("@") === -1 ||
+        frm.email.valueOf.indexOf(".") === -1 ||
+        frm.email.value === "" ||
         frm.email.value == null) {
         alert("Por favor, indique um e-mail válido.");
         frm.email.focus();
         return false;
     }
-    // O utilizador necessita de selecionar um dos dois
-    //radio buttons: Masculino ou Feminino.
-    let escolhaSexo = -1; //valor negativo default (padrão) que significa que nada foi escolhido ainda.
-    //No bloco de código abaixo foi criado um ciclo entre
-    //os radios button com o mesmo nome (sexo)
-    for(let x = frm.sexo.length-1; x > -1; x--) {
-        /*
-        x = frm.sexo.lenght -1 é a mesma coisa que: x = 2-
-        1, que resulta em 1.
-        x > -1 significa que o valor de x não pode ser igual a -1 e
-        sim maior, porque -1 significa que nada foi escolhido.
-        x-- significa que há um decremento no valor x, é algo como:
-        x = 1, x= 0 e para, pois x não pode ser -1.
-        */
-        if(frm.sexo[x].checked) { //checked quer dizer selecionado,
-            //então verifica se o primeiro (0) ou o
-            //segundo (1) radio button foi selecionado (checked).
-            escolhaSexo = x; //atribui à variável escolhaSexo o valor X.
-        }
-    }
-    //se nenhuma das opções (masculino ou feminino) forem
-    //escolhidas, mostra um alerta e cancela o envio.
-    if(escolhaSexo == -1) {
-        alert("Qual o seu Sexo?");
-        frm.sexo[0].focus();
-        return false;
-    }
-    //o campo telefone não pode estar vazio pelo menos
-    if(frm.tel.value == "" || frm.tel.value == null) {
-        //É mostrado um alerta, caso o campo esteja vazio.
-        alert("Por favor, indique o seu telemóvel.");
-        //Foi definido um focus no campo.
-        frm.tel.focus();
-        //o form não é enviado.
-        return false;
-    }
-    //Valida a textArea, que é como validar um campo de texto simples.
-    if(frm.ass.value == "" || frm.ass.value == null) {
+
+    //Valida o select sobre o assunto desejado
+    if(frm.ass.option[frm.ass.selectedIndex].value === "") {
         alert("Por favor, coloque um assunto para podermos sanar sua dúvida melhor.");
-        frm.conteudo.focus();
+        frm.ass.focus();
         return false;
     }
+
     //Valida a textArea, que é como validar um campo de texto simples.
-    if(frm.conteudo.value == "" || frm.conteudo.value == null) {
+    if(frm.conteudo.value === "" || frm.conteudo.value == null) {
         alert("Por favor, coloque algum conteúdo para sanar sua dúvida melhor.");
         frm.conteudo.focus();
         return false;
