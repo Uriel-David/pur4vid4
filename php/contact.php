2d9754bba2925daa8a0433a5e0e1e51b5d355dde
Uriel-David <uriel.david.qaa@gmail.com>
2020-05-01 18:20:25 -0300

    Ajustes na página de contato principalmente no arquivo contato.php e adição do server XAMPP ao projeto.

--- php/contact.php
+++ php/contact.php
@@ -15,10 +15,11 @@ $mail = new PHPMailer();
 $mail -> IsSMTP();
 
 // Enviar por SMTP
-$mail -> Host = "mail.gmail.com";
+$mail -> Host = "smtp.gmail.com";
 
 // Você pode alterar este parametro para o endereço de SMTP do seu provedor
-$mail->Port = 587;
+$mail -> SMTPSecure = 'tls';
+$mail -> Port = 587;
 
 
 // Usar autenticação SMTP (obrigatório)
@@ -26,20 +27,18 @@ $mail -> SMTPAuth = true;
 
 // Usuário do servidor SMTP (endereço de email)
 // obs: Use a mesma senha da sua conta de email
-$mail->Username = 'uriel.david.qaa@gmail.com';
-$mail->Password = 'Purumal10!';
+$mail -> Username = 'uriel.david.qaa@gmail.com';
+$mail -> Password = 'Purumal10!';
 
 // Configurações de compatibilidade para autenticação em TLS
 $mail -> SMTPOptions = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ) );
 
 // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro.
-// $mail->SMTPDebug = 2;
+//$mail->SMTPDebug = 2;
 
 // Define o remetente
-// Seu e-mail
-$mail -> From = "$email";
-
-// Seu nome
+// Seu e-mail e nome
+//$mail -> From = "$email";
 $mail -> FromName = "$nome";
 
 // Define o(s) destinatário(s)
@@ -54,16 +53,16 @@ $mail -> AddAddress('uriel.david.qaa@gmail.com', 'Pur4Vid4 Surfboards');
 
 // Definir se o e-mail é em formato HTML ou texto plano
 // Formato HTML . Use "false" para enviar em formato texto simples ou "true" para HTML.
-$mail -> IsHTML(false);
+$mail -> IsHTML(true);
 
 // Charset (opcional)
 $mail -> CharSet = 'UTF-8';
 
 // Assunto da mensagem
-$mail->Subject = "$ass";
+$mail -> Subject = "$ass";
 
 // Corpo do email
-$mail -> Body = "$conteudo";
+$mail -> Body = "$conteudo + $email";
 
 // Opcional: Anexos
 // $mail -> AddAttachment("/home/usuario/public_html/documento.pdf", "documento.pdf");
